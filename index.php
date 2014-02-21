c8222be5670c208b8825a15636b33de7939235ce
Knewtn <email@knewt.se>
2014-02-21 15:19:18 +0100

    Changed font (Roboto) + added typography examples

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ include_once('php/general.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
 	<link href="css/style.css" rel="stylesheet">
 	<link href="css/responsive.css" rel="stylesheet">
 
@@ -107,14 +108,17 @@ include_once('php/general.php');
 				  </div>
 		 	 </div> 
 			 <div class="top-div"></div>
-   		 	 <div class="row">
-				 <div class="col-sm-12">
+   		 	 <!--<div class="row">
+				 
+				 KOMMANDE PASS = ONÖDIGT?
+					 
+					 <div class="col-sm-12">
 				 	<h4 style="font-weight: 200;">Kommande pass</h4>
 					<?php
 						loadUpcomingEvents($date);
 					?>
 			 	 </div>
-   		 	 </div>
+   		 	 </div> -->
    		 	 <div class="row">
 				 <div class="col-sm-5">
 					 <div class="row">
@@ -124,14 +128,19 @@ include_once('php/general.php');
 						 <div class="col-sm-12"><h4>Lediga pass</h4></div>
 				 	</div>
 				 </div>
-				 <div class="col-sm-7"><h1 class="thin-100">Ny termin och nya arbetspass!</h1>
+				 <div class="col-sm-7">
+					 <h1>En helgrym H1-rubrik. Light 36px</h1>
 
-					 <p>Hej! Terminen har dragit igång och det finns några pass kvar i januari att fylla. Framförallt på Vinterkravallen och Nollefesten, där behöver vi dessutom allapass som vem som helst kan jobba som! Annars finns det nu pass för februari, och som vanligt gäller det att jobba 8 poäng eller motsvarande för att få personalförmåner. Nästa personalfest är i mars, så för att få gå på den måste man ha jobbat sina poäng i både dec/jan och februari! Alternativt vara nybyggare och aldrig ha gått på en personalfest innan.
-
+					 <p>Hej! Terminen har dragit igång och det finns några pass kvar i januari att fylla. Framförallt på Vinterkravallen och Nollefesten, där behöver vi dessutom allapass som vem som helst kan jobba som! Annars finns det nu pass för februari, och som vanligt gäller det att jobba 8 poäng eller motsvarande för att få personalförmåner. 
+						 
+<h2>En H2-rubrik som är smal. Light 30px</h2>
 Ny termin betyder också ny personal, onsdagen den 5/2 kl 18 kommer vi ha infokväll på Trappan! Så om ni känner någon som är sugen på att börja men vill ha mer information om lagen och Trappan - tipsa dem om infokvällen!
 
+<h3>Sen har vi H3 också. Light 24px</h3>
+<h4>Och en H4. Bold 18px</h4>
 Dagen innan infokvällen, alltså tisdagen 4/2, vill vi istället hylla er som redan jobbar på Trappan genom att ha personalpub! Denna gång är det marknadsföringslaget och barlaget som ska anordna, mer information kommer inom kort.Hej! Terminen har dragit igång och det finns några pass kvar i januari att fylla. Framförallt på Vinterkravallen och Nollefesten, där behöver vi dessutom allapass som vem som helst kan jobba som! Annars finns det nu pass för februari, och som vanligt gäller det att jobba 8 poäng eller motsvarande för att få personalförmåner. Nästa personalfest är i mars, så för att få gå på den måste man ha jobbat sina poäng i både dec/jan och februari! Alternativt vara nybyggare och aldrig ha gått på en personalfest innan.
-
+<h5>En H5-rubrik kommer här. Bold 14px</h5>
+<h6>En H6-rubrik kommer här. Bold 12px</h6>
 Ny termin betyder också ny personal, onsdagen den 5/2 kl 18 kommer vi ha infokväll på Trappan! Så om ni känner någon som är sugen på att börja men vill ha mer information om lagen och Trappan - tipsa dem om infokvällen!
 
 Dagen innan infokvällen, alltså tisdagen 4/2, vill vi istället hylla er som redan jobbar på Trappan genom att ha personalpub! Denna gång är det marknadsföringslaget och barlaget som ska anordna, mer information kommer inom kort</p></div>
