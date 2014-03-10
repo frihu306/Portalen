334e8686303349da829b6858e11ae5faf9aafc98
Knewtn <email@knewt.se>
2014-03-10 16:39:21 +0100

    "Om portalen" page, not done yet though

--- pages/about.php
+++ pages/about.php
@@ -1,3 +1,55 @@
-<div class="col-sm-5">
-	<p>Portalen byggdes om av webblaget 13/14 under våren 2014.</p>
+
+	<div class="col-sm-12">
+<h1>Om Personalportalen</h1>
+	</div>
+</div> <!-- .row -->
+
+<div class="row">
+	
+<div class="col-sm-6">
+	<p>Utvecklare, nördar och gudar. Trappans webblag har många smeknamn. Och det är just denna samling av människor, alltså Trappans webblag 2014, som tagit fram Personalportalen.<br /><br /> Har du några frågor eller synpunkter angående portalen eller kanske bara känner för att prata med helgrymma människor så går det alldeles utmärkt att kontakta någon i Trappans <a href="">webblag</a>, förslagsvis <a href="">lagchefen</a>.</p>
+	<h4>Glyphicons</h4>
+	<p>hejhej</p>
+	<h4>Gamla Personalportalen</h4>
+	<p>Den gamla Personalportalen användes från 2007 fram till 2014 och utvecklades av Johan Åkesson och Tobias Johansson. Många av den gamla portalens funktioner och kod används även i den nya portalen, men mycket har finjusterats, tagits bort och lagts till. Utseendet på de olika portalerna skiljer sig även avsevärt.</p>
+</div>
+<div class="col-sm-6">
+	<h2>Vi har utvecklat portalen</h2>
+	<div class="media">
+	  <a class="pull-left" href="#">
+	    <img class="media-object avatar-96x96" src="https://fbcdn-sphotos-a-a.akamaihd.net/hphotos-ak-prn2/t31/1796073_721213827918428_191568827_o.jpg" width="96px" height="96px" alt="namn">
+	  </a>
+	  <div class="media-body">
+	    <h4 class="media-heading">Hampus Axelsson</h4>
+	    <p>fem MT-år på CV:t! Livet leker och jag äger en rökmaskin. Har varit webbansvarig hur länge som helst, typ flera år! Vann en rätt najs tävling för inte så länge sen. Det var ett spelutvecklingsgrejs. Sums it up pretty much yes</p>
+	  </div>
+	</div>
+	<div class="media">
+	  <a class="pull-left" href="#">
+	    <img class="media-object avatar-96x96" src="https://fbcdn-sphotos-g-a.akamaihd.net/hphotos-ak-frc1/t1/1924731_10203615874384240_1441100208_n.jpg" width="96px" height="96px" alt="namn">
+	  </a>
+	  <div class="media-body">
+	    <h3 class="media-heading">Andreas Antonsson</h3>
+	    <p>Pluggar GDK yao. fett bra människa. Vacker. Rolig. Jättespontan. Världsvan. Ödmjuk. Växt upp med HTML, foppoll, Ford Fiesta och en fobi mot frukt och grönsaker (men diggar grönt).</p>
+	  </div>
+	</div>
+	<div class="media">
+	  <a class="pull-left" href="#">
+	    <img class="media-object avatar-96x96" src="https://scontent-a.xx.fbcdn.net/hphotos-ash3/t1/1948036_10203333887416234_1102924100_n.jpg" width="96px" height="96px" alt="namn">
+	  </a>
+	  <div class="media-body">
+	    <h3 class="media-heading">Connie Åberg</h3>
+	    <p>Bor på ett fett najs studentboende. Gör resten av webblaget avundsjuka. Kommer från Gotland, där finns det får. Rätt stor Ö ändå. Rauuukar. Jättelång text tänkte jag att detta skulle va, men jag kommer inte på mer roliga saker att skriva. Connie har samma efternamn som Alfons, Alfons Åberg!</p>
+	  </div>
+	</div>
+	<div class="media">
+	  <a class="pull-left" href="#">
+	    <img class="media-object avatar-96x96" src="https://scontent-a.xx.fbcdn.net/hphotos-ash3/t1/77153_10203151144085215_1474897962_n.jpg" width="96px" height="96px" alt="namn">
+	  </a>
+	  <div class="media-body">
+	    <h3 class="media-heading">Linus Sjölinder</h3>
+	    <p>Ankor och fulvin. Väcker Pontus på våning 4 med hård döttstechno. Jag behöver egentligen inte skriva så mycket om Linus, för han placerades längst ner så för exempel-layoutens skull är det inte nödvändigt med stort textblock här. Men nu när jag förklarat allt detta så blev texten lång i alla fall. I äm fabulous.</p>
+	  </div>
+	</div>
+	
 </div>
