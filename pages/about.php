1e39ab5127fed70769a9aa8598228a14764b97ef
Knewtn <email@knewt.se>
2014-03-11 11:47:53 +0100

    "Om portalen" page again

--- pages/about.php
+++ pages/about.php
@@ -1,6 +1,6 @@
 
 	<div class="col-sm-12">
-<h1>Om Personalportalen</h1>
+			<h1>Om Personalportalen</h1>
 	</div>
 </div> <!-- .row -->
 
@@ -9,18 +9,17 @@
 <div class="col-sm-6">
 	<p>Utvecklare, nördar och gudar. Trappans webblag har många smeknamn. Och det är just denna samling av människor, alltså Trappans webblag 2014, som tagit fram Personalportalen.<br /><br /> Har du några frågor eller synpunkter angående portalen eller kanske bara känner för att prata med helgrymma människor så går det alldeles utmärkt att kontakta någon i Trappans <a href="">webblag</a>, förslagsvis <a href="">lagchefen</a>.</p>
 	<h4>Glyphicons</h4>
-	<p>hejhej</p>
+	<p>I och med att portalen använder sig av Bootstrap 3.0 har vi haft förmånen att använda oss av ikoner från Glyphicon Halflings set. Detta set är egentligen inte gratis, men har gjorts tillgängliga just för Bootstrap. Så det hör lite till god sed att länka tillbaka till Glyphicons och ge dem cred. Och det gör vi lite här och var. <a href="http://glyphicons.com">Som här till exempel</a>.</p>
 	<h4>Gamla Personalportalen</h4>
-	<p>Den gamla Personalportalen användes från 2007 fram till 2014 och utvecklades av Johan Åkesson och Tobias Johansson. Många av den gamla portalens funktioner och kod används även i den nya portalen, men mycket har finjusterats, tagits bort och lagts till. Utseendet på de olika portalerna skiljer sig även avsevärt.</p>
+	<p>Den gamla Personalportalen användes från 2007 fram till 2014 och utvecklades av Johan Åkesson och Tobias Johansson. Många av den gamla portalens funktioner och kod används även i den nya portalen, men mycket har finjusterats, tagits bort och lagts till. Utseendet på den nya portalen skiljer sig även avsevärt.</p>
 </div>
 <div class="col-sm-6">
-	<h2>Vi har utvecklat portalen</h2>
 	<div class="media">
 	  <a class="pull-left" href="#">
 	    <img class="media-object avatar-96x96" src="https://fbcdn-sphotos-a-a.akamaihd.net/hphotos-ak-prn2/t31/1796073_721213827918428_191568827_o.jpg" width="96px" height="96px" alt="namn">
 	  </a>
 	  <div class="media-body">
-	    <h4 class="media-heading">Hampus Axelsson</h4>
+	    <h3 class="media-heading">Hampus Axelsson</h3>
 	    <p>fem MT-år på CV:t! Livet leker och jag äger en rökmaskin. Har varit webbansvarig hur länge som helst, typ flera år! Vann en rätt najs tävling för inte så länge sen. Det var ett spelutvecklingsgrejs. Sums it up pretty much yes</p>
 	  </div>
 	</div>
@@ -39,7 +38,7 @@
 	  </a>
 	  <div class="media-body">
 	    <h3 class="media-heading">Connie Åberg</h3>
-	    <p>Bor på ett fett najs studentboende. Gör resten av webblaget avundsjuka. Kommer från Gotland, där finns det får. Rätt stor Ö ändå. Rauuukar. Jättelång text tänkte jag att detta skulle va, men jag kommer inte på mer roliga saker att skriva. Connie har samma efternamn som Alfons, Alfons Åberg!</p>
+	    <p>Bor på ett fett najs studentboende. Gör resten av webblaget avundsjuka. Kommer från Gotland, där finns det får. Rätt stor Ö ändå. Oklart varför jag skrev ö med versalt ö. Jättelång text tänkte jag att detta skulle va, men jag kommer inte på mer roliga saker att skriva. Connie har samma efternamn som Alfons, Alfons Åberg!</p>
 	  </div>
 	</div>
 	<div class="media">
