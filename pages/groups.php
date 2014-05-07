fd5c011ff9d3a66fbba19dc9a0764d90016c79dd
asdasd <asdasd@asdasd>
2014-05-07 16:39:44 +0200

    Grouppages

--- pages/groups.php
+++ pages/groups.php
@@ -1,6 +1,6 @@
 	<div class="col-sm-12">
 		<div class="page-header">
-			<h1>Lag</h1>
+			<h1>Lagsidor</h1>
 		</div>
 	</div>
 </div> <!-- .row -->
@@ -8,7 +8,7 @@
 <div class="row">
 
 <div class="col-sm-5">
-	<p>Det finns många lag...</p>
+	<img src="<?php echo loadAvatar(); ?>" class="avatar-96x96" width="96px" height="96px"><p>Vi i Webblaget ser till att Trappans hemsidor fungerar. Både den externa för gästerna och den interna portalen för personalen. </br>Har du det som krävs för att vidareutveckla Trappan på webben? Tveka inte! Sök!</p>
 </div>
 <div class="col-sm-7">
 	<p>Men webblaget är det bästa!</p>
